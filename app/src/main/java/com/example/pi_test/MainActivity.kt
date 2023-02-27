02846a44eb97108d9af9caaf80a131b28e1c706e
Iter0513 <kevin000513@naver.com>
2023-02-27 16:57:03 +0900

    git commit

---
+++ app/src/main/java/com/example/pi_test/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.pi_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_2)
+
+
+    }
+}
